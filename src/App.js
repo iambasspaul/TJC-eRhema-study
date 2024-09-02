7e53ad2c7fd91ca1afc864069dd6f9e7f2466b70
PaulHuang <iambasspaul@gmail.com>
2024-09-02 17:28:54 +0800

    update leftcolumn and app.js

--- src/App.js
+++ src/App.js
@@ -5,25 +5,25 @@ import RightColumn from './components/RightColumn';
 import './App.css';
 
 const App = () => {
-    const [selectedWord, setSelectedWord] = useState(null);
+  const [selectedWord, setSelectedWord] = useState(null);
 
-    const handleSelectWord = (book, chapter, verseNumber, strongID, word, monad) => {
-        setSelectedWord({ book, chapter, verseNumber, strongID, word, monad });
-    };
+  const handleSelectWord = (book, chapter, verseNumber, strongID, word, monad) => {
+    setSelectedWord({ book, chapter, verseNumber, strongID, word, monad });
+  };
 
-    return (
-        <div className="app-container">
-            <Header />
-            <div className="columns-container">
-                <div className="column left-column">
-                    <LeftColumn onSelectVerse={handleSelectWord} />
-                </div>
-                <div className="column right-column">
-                    <RightColumn selectedWord={selectedWord} />
-                </div>
-            </div>
+  return (
+    <div className="app-container">
+      <Header />
+      <div className="columns-container">
+        <div className="column left-column">
+          <LeftColumn onSelectVerse={handleSelectWord} />
         </div>
-    );
+        <div className="column right-column">
+          <RightColumn selectedWord={selectedWord} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
--- src/components/LeftColumn.js
+++ src/components/LeftColumn.js
@@ -1,78 +1,71 @@
 import React, { useEffect, useState } from 'react';
 
-const LeftColumn = ({ onSelectVerse }) => {
+const LeftColumn = ({ onSelectVerse, selectedWord }) => {
   const [verses, setVerses] = useState({});
-  const [selectedWord, setSelectedWord] = useState(null);
-  const [BookNameLookup, setBookNameLookup] = useState({});
-  
+  const [bookNameLookup, setBookNameLookup] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     // Fetch Bible verses
     fetch('/Bible_BSB_NT_40.json')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(data => {
-            // Ensure the data structure is as expected
-            const versesContent = data.content;
-            if (versesContent && typeof versesContent === 'object') {
-                setVerses(versesContent);
-            } else {
-                console.error('Fetched data is not in the expected format:', versesContent);
-            }
-        })
-        .catch(error => {
-            console.error('There was a problem with the fetch operation:', error);
-        });
+      .then(response => response.json())
+      .then(data => {
+        // Ensure the data structure is as expected
+        if (data && typeof data === 'object' && data.content) {
+          setVerses(data.content);
+          setIsLoading(false);
+        } else {
+          console.error('Fetched data is not in the expected format:', data);
+          setIsLoading(false);
+        }
+      })
+      .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+        setIsLoading(false);
+      });
 
     // Fetch book names
     fetch('/book_lookup.json')
-        .then(response => response.json())
-        .then(data => setBookNameLookup(data.fullname.bsb.en))
-        .catch(error => console.error('Error fetching book lookup:', error));
-
+      .then(response => response.json())
+      .then(data => setBookNameLookup(data.fullname.bsb.en))
+      .catch(error => console.error('Error fetching book lookup:', error));
   }, []);
 
   const handleWordSelect = (wordDetails) => {
-    setSelectedWord(wordDetails);
     onSelectVerse(wordDetails.book, wordDetails.chapter, wordDetails.verseNumber, wordDetails.strongID, wordDetails.word, wordDetails.monad);
   };
 
-  
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
   return (
-      <div style={{ width: '50%', overflowY: 'scroll' }}>
-          {Object.entries(verses).map(([book, chapters]) => (
-              <div key={book}>
-                  <h2>Book {BookNameLookup[book] || book}</h2>
-                  {Object.entries(chapters).map(([chapter, verses]) => (
-                      <div key={chapter}>
-                          <h3>Chapter {chapter}</h3>
-                          {Object.entries(verses).map(([verseNumber, verseArray]) => (
-                              <p key={verseNumber}>
-                                <strong>verse {verseNumber} </strong>
-                                  {verseArray.map((wordArray) => {
-                                      const [strongID, word, monad] = wordArray;
-                                      return (
-                                          <span
-                                              key={strongID}
-                                              onMouseDown={() => handleWordSelect({ book, chapter, verseNumber, strongID, word, monad })}
-                                              style={{ backgroundColor: selectedWord?.strongID === strongID ? 'yellow' : 'transparent' }}
-                                          >
-                                              {word}
-                                          </span>
-                                      );
-                                  })}
-                              </p>
-                          ))}
-                      </div>
+    <div style={{ width: '50%', overflowY: 'scroll' }}>
+      {Object.entries(verses).map(([book, chapters]) => (
+        <div key={book}>
+          <h2>Book {bookNameLookup[book] || book}</h2>
+          {Object.entries(chapters).map(([chapter, verseArray]) => (
+            <div key={chapter}>
+              <h3>Chapter {chapter}</h3>
+              {Object.entries(verseArray).map(([verseNumber, wordArrays]) => (
+                <p key={verseNumber}>
+                  <strong>Verse {verseNumber}</strong>
+                  {wordArrays.map(([strongID, word, monad]) => (
+                    <span
+                      key={strongID}
+                      onMouseDown={() => handleWordSelect({ book, chapter, verseNumber, strongID, word, monad })}
+                      style={{ backgroundColor: selectedWord?.strongID === strongID ? 'yellow' : 'transparent' }}
+                    >
+                      {word}
+                    </span>
                   ))}
-              </div>
+                </p>
+              ))}
+            </div>
           ))}
-      </div>
+        </div>
+      ))}
+    </div>
   );
 };
 
